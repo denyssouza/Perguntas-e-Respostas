d17d6db212670a4b8bf618db69efbcc589c779cf
Denys Souza <denys.souza@dks.net.br>
2018-05-19 00:02:23 -0300

    Recebendo nome de Player2 via socket.

--- src/AppPerguntaERespostas.java
+++ src/AppPerguntaERespostas.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
@@ -13,10 +14,10 @@ public class AppPerguntaERespostas {
         ServerSocket InputServidor = new ServerSocket(4242); // Definido a porta 4242 TCP.
         System.out.println("Servidor Iniciado...");
         Socket InputClient = InputServidor.accept();
-        System.out.println("Conexão Estabelecida. " + InputClient.getInetAddress().getHostAddress()); // Apresenta IP e Host.
+        System.out.println("Conexão Estabelecida com: " + InputClient.getInetAddress().getHostAddress()); // Apresenta IP e Host.
 
 
-        // Recebendo Nome dos Jogadores
+        // Recebendo Nome Player 1 Local
 
         Scanner entrada = new Scanner(System.in);
         Players player = new Players();
@@ -24,8 +25,17 @@ public class AppPerguntaERespostas {
         System.out.println("Informe o nome do Player 1: ");
         player.setPlayer1(entrada.next());
 
-        System.out.println("Informe o nome do Player 2: ");
-        player.setPlayer2(entrada.next());
+        // Recebendo Nome Player 2 Via Socket
+
+        Scanner s = new Scanner(InputClient.getInputStream());
+        player.setPlayer2(s.nextLine());
+
+        // Comentário
+
+
+        //PrintStream saida = new PrintStream(InputClient.getOutputStream());
+        //Scanner teclado = new Scanner(System.in);
+        //saida.println(teclado.nextLine());
 
         player.NamePlayers();
     }
--- src/AppPerguntasERespostasClient.java
+++ src/AppPerguntasERespostasClient.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class AppPerguntasERespostasClient {
 
@@ -10,5 +13,20 @@ public class AppPerguntasERespostasClient {
 
         Socket OutServidor = new Socket("127.0.0.1", 4242); // Conectando ao IP localHost:4242
         System.out.println("Conexão Estabelecida.");
+
+        // Enviando nome do Player 2 via Socket
+
+        System.out.println("Informe o nome do Player 2: ");
+        Scanner teclado = new Scanner(System.in);
+        PrintStream saida = new PrintStream(OutServidor.getOutputStream());
+        saida.println(teclado.nextLine());
+
+        // Comentário
+
+        //ObjectInputStream entrada = new ObjectInputStream(OutServidor.getInputStream());
+        //Scanner s = new Scanner(OutServidor.getInputStream());
+        //System.out.println(s.nextLine());
+
+
     }
 }
